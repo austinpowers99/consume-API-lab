65d70f23b88c38f09a8c99122a08509b5210dbd3
austinpowers99 <samanthaf_9@outlook.com>
2023-07-06 19:07:56 -0700

    update

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-const ROOT_URL = 'https://api.chucknorris.io/jokes';
-const URL_CATEGORY = 'https://api.chucknorris.io/jokes/categories';
+const ROOT_URL = 'https://api.chucknorris.io/jokes/random';
+// const URL_CATEGORY = 'https://api.chucknorris.io/jokes/categories';
 
 router.get('/', async function(req, res, next) {
-  let joke;
-  const categories = await fetch(`${URL_CATEGORY}`)
+  const getJoke = await fetch(`${ROOT_URL}`)
     .then(res => res.json());
+  const joke = getJoke.value;
+  console.log(joke)
 
-    if (req.query) {
-      const category = req.query.category;
-      joke = await fetch(`${ROOT_URL}/random?category=${category}`)
-        .then(res => res.json())
-    }
+    // if (req.query) {
+    //   const category = req.query.category;
+    //   joke = await fetch(`${ROOT_URL}/random?category=${category}`)
+    //     .then(res => res.json())
+    // }
   res.render('index', { title: 'Chuck Norris Joke Generator', joke});
 });
 
